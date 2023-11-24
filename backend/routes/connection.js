2c05cb63988d4371e7e81fc939096dd0c8d30080
jlauza <lauzajayson@gmail.com>
2023-11-24 11:14:32 +0800

    use dotenv

--- backend/routes/connection.js
+++ backend/routes/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-// const mongoURI = process.env.MONGO_URI;
-const mongoURI = "mongodb://localhost:27017/antcode";
-console.log(mongoURI);
+const mongoURI = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {

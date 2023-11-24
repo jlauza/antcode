2e5dd1d4d182a159cc60e0e7154325e3c4bbdc66
jlauza <lauzajayson@gmail.com>
2023-11-24 11:07:11 +0800

    added more method and endpoint

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// GET all users
+router.get("/", function (req, res, next) {
+  // Logic to fetch and send list of users
+  res.send("Respond with a resource");
+});
+
+// GET a specific user by ID
+router.get("/:id", (req, res) => {
+  // Logic to fetch and send details of a specific user
+  // Access the user ID with req.params.id
+  res.send("Get user with ID: " + req.params.id);
+});
+
+// POST a new user
+router.post("/", (req, res) => {
+  // Logic to create a new user with data from req.body
+  res.send("Register user");
+});
+
+// PUT update a specific user by ID
+router.put("/:id", (req, res) => {
+  // Logic to update a user identified by req.params.id with data from req.body
+  res.send("Update user with ID: " + req.params.id);
+});
+
+// DELETE a specific user by ID
+router.delete("/:id", (req, res) => {
+  // Logic to delete a user identified by req.params.id
+  res.send("Delete user with ID: " + req.params.id);
 });
 
 module.exports = router;

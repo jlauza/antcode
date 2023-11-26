f6a388349ff4dab91f35f1b82fe659ed129b526d
jlauza <lauzajayson@gmail.com>
2023-11-27 00:22:35 +0800

    get profile

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const User = require("../models/user.model");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -8,6 +9,24 @@ router.get("/", function (req, res, next) {
 
 router.get("/register", function (req, res, next) {
   res.render("register", { title: "Register" });
+});
+
+router.get("/users/profile/:id", async function (req, res, next) {
+  // Logic to fetch and send details of a specific user
+  const user = await User.findById(req.params.id).select("-password").exec();
+
+  res.render("profile", {
+    title: "My Profile",
+    id: user._id,
+    firstname: user.firstname,
+    lastname: user.lastname,
+    email: user.email,
+    avatar: user.avatar
+      ? user.avatar
+      : "https://placehold.co/120x120?text=No+Photo",
+    bio: user.bio ? user.bio : "No bio",
+    role: user.role,
+  });
 });
 
 module.exports = router;

373b4d2f19850fba163dbeabb0d27f675f716b70
jlauza <lauzajayson@gmail.com>
2024-01-20 00:03:01 +0800

    added new route

--- frontend/src/ComponentRouter.js
+++ frontend/src/ComponentRouter.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Menu } from "antd";
 import Home from "./Pages/Home";
+import About from "./Pages/About";
 
 const ComponentRouter = () => {
   return (
@@ -10,10 +11,14 @@ const ComponentRouter = () => {
         <Menu.Item key="home">
           <Link to="/">Home</Link>
         </Menu.Item>
+        <Menu.Item key="about">
+          <Link to="/about">About</Link>
+        </Menu.Item>
       </Menu>
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/Pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>Welcome to the About page!</h1>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default About;

f185fed52abadff352f25fc7d6b950af83f6d238
jlauza <lauzajayson@gmail.com>
2024-01-26 12:17:33 +0800

    fixed menu

--- frontend/src/ComponentRouter.js
+++ frontend/src/ComponentRouter.js
@@ -1,24 +1,46 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
 import { Menu } from "antd";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import Login from "./Features/Auth/Login";
+
+function NavigationMenu() {
+  return (
+    <Menu
+      mode="horizontal"
+      style={{ display: "flex", justifyContent: "space-between" }}
+    >
+      <Menu.Item key="/">
+        <Link to="/">Home</Link>
+      </Menu.Item>
+      <Menu.Item key="/about">
+        <Link to="/about">About</Link>
+      </Menu.Item>
+
+      <Menu.Item key="/login" style={{ marginLeft: "auto" }}>
+        <Link to="/login">Login</Link>
+      </Menu.Item>
+    </Menu>
+  );
+}
 
 const ComponentRouter = () => {
   return (
     <Router>
-      <Menu mode="horizontal">
-        <Menu.Item key="home">
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Item key="about">
-          <Link to="/about">About</Link>
-        </Menu.Item>
-      </Menu>
+      <NavigationMenu />
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        {/* Add a Route for the login page if you have one */}
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/Features/Auth/Login.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Form, Input, Button } from "antd";
+
+const Login = () => {
+  const onFinish = (values) => {
+    console.log("Received values:", values);
+  };
+
+  return (
+    <Form
+      name="login-form"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: "Please input your username!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Log in
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Login;

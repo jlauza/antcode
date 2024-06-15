dfa57c4b9a9cccf6fbef6f80610d9c883cab4037
jlauza <lauzajayson@gmail.com>
2024-06-15 15:19:10 +0800

    added var for express session

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
+var session = require("express-session");
 var logger = require("morgan");
 var cors = require("cors");
 var multer = require("multer");

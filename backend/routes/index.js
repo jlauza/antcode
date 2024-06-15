9fd067f4d9fe79ef6971c1dc180bfec9b1ca4c77
jlauza <lauzajayson@gmail.com>
2024-06-15 15:53:44 +0800

    implement to routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,6 +6,7 @@ var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var multer = require("multer");
 var upload = multer();
+const ensureAuthenticated = require("../backend/routes/authMiddleware");
 
 router.get("/login", async function (req, res) {
   // Render login page
@@ -26,18 +27,18 @@ router.get("/login", async function (req, res) {
   }
 });
 
-function checkSignIn(req, res) {
-  if (req.session?.user) {
-    // next(); //If session exists, proceed to page
-    res.redirect(`/dashboard`);
-    res.render("dashboard", { title: "Dashboard" });
-  } else {
-    // var err = new Error("Not logged in!");
-    // next(err);
-    res.redirect(`/login`);
-    res.render("login", { message: "Invalid credentials" });
-  }
-}
+// function checkSignIn(req, res) {
+//   if (req.session?.user) {
+//     // next(); //If session exists, proceed to page
+//     res.redirect(`/dashboard`);
+//     res.render("dashboard", { title: "Dashboard" });
+//   } else {
+//     // var err = new Error("Not logged in!");
+//     // next(err);
+//     res.redirect(`/login`);
+//     res.render("login", { message: "Invalid credentials" });
+//   }
+// }
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -48,32 +49,36 @@ router.get("/register", function (req, res, next) {
   res.render("register", { title: "Register" });
 });
 
-router.get("/users/profile/:id", checkSignIn, async function (req, res, next) {
-  // Logic to fetch and send details of a specific user
-  const user = await User.findById(req.params.id).select("-password").exec();
+router.get(
+  "/users/profile/:id",
+  ensureAuthenticated,
+  async function (req, res, next) {
+    // Logic to fetch and send details of a specific user
+    const user = await User.findById(req.params.id).select("-password").exec();
 
-  if (!user) {
-    // return res.status(404).json({ message: "User not found" });
-    return res.render("not-found", {
-      title: "User not found",
-      message: "The page you are looking for does not exist.",
+    if (!user) {
+      // return res.status(404).json({ message: "User not found" });
+      return res.render("not-found", {
+        title: "User not found",
+        message: "The page you are looking for does not exist.",
+      });
+    }
+
+    res.render("profile", {
+      title: "My Profile",
+      id: user._id,
+      username: user.username,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      email: user.email,
+      role: user.role,
     });
   }
-
-  res.render("profile", {
-    title: "My Profile",
-    id: user._id,
-    username: user.username,
-    firstname: user.firstname,
-    lastname: user.lastname,
-    email: user.email,
-    role: user.role,
-  });
-});
+);
 
 router.get(
   "/users/profile/:username",
-  checkSignIn,
+  ensureAuthenticated,
   async function (req, res, next) {
     // Logic to fetch and send details of a specific user
     const user = await User.findOne({
@@ -96,7 +101,7 @@ router.get(
 
 router.get(
   "/users/profile/edit/:id",
-  checkSignIn,
+  ensureAuthenticated,
   async function (req, res, next) {
     // Logic to fetch and send details of a specific user
     const user = await User.findById({
@@ -119,7 +124,7 @@ router.get(
 
 router.get(
   "/users/profile/delete/:id",
-  checkSignIn,
+  ensureAuthenticated,
   async function (req, res, next) {
     const user = await User.findById({
       _id: req.params.id,
@@ -138,7 +143,7 @@ router.get(
 );
 
 // Get Dashboard page
-router.get("/dashboard", function (req, res, next) {
+router.get("/dashboard", ensureAuthenticated, function (req, res, next) {
   res.render("dashboard", { title: "My Dashboard" });
 });
 

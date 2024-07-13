99c98d47b5e05c0a07d087fbf11bb70ae462b82c
jlauza <lauzajayson@gmail.com>
2024-07-14 00:20:18 +0800

    create blocks and draft try catch

--- frontend/src/Features/Auth/useAuthService.js
+++ frontend/src/Features/Auth/useAuthService.js
@@ -1,10 +1,25 @@
 import axios from "axios";
+import { useState } from "react";
 const apiURL = process.env.REACT_APP_API_USERS;
 
-console.log(apiURL);
-
 const useAuthService = () => {
-  //
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const authenticate = async (values) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      // fetch data is existing using axios
+    } catch (err) {
+      setLoading(false);
+      setError(err.message);
+      throw err;
+    }
+  };
+
+  return { authenticate, isLoading, error };
 };
 
 export default useAuthService;

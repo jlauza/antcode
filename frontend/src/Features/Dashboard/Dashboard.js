4c4f7c749d66b8efe9b39c70d0adfe34e206352c
jlauza <lauzajayson@gmail.com>
2024-07-14 12:44:02 +0800

    draft dashboard

---
+++ frontend/src/Features/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Welcome to Dashboard</h1>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default Dashboard;

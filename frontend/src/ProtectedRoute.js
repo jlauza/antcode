caa02543b5df160dc0f338fc631ac6a7afaf75ae
jlauza <lauzajayson@gmail.com>
2024-07-19 18:55:42 +0800

    created protected route

--- frontend/src/ProtectedRoute.js
+++ frontend/src/ProtectedRoute.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useAuth } from "./Context/AuthContext";
 
-const ProtectedRoute = () => {};
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const { isAuthenticated } = useAuth();
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />
+      }
+    />
+  );
+};
 
 export default ProtectedRoute;

d01bfda539e4bc10b4ac42fff365c680deddfbc7
jlauza <lauzajayson@gmail.com>
2024-09-23 19:42:44 +0800

    add new user js and env

---
+++ public/javascripts/add-user.js
@@ -0,0 +1,34 @@
+function registerUser(event) {
+  event.preventDefault();
+  const form = document.getElementById("register-form");
+  const formData = new FormData(form);
+
+  fetch(form.action, {
+    method: "POST",
+    body: formData,
+  })
+    .then(async (response) => {
+      const data = await response.json();
+
+      if (response.ok) {
+        window.location.href = "/dashboard";
+      }
+
+      if (!response.ok) {
+        const errorMessage = document.getElementById("error-message");
+
+        if (response.status) {
+          errorMessage.textContent = data.error;
+        }
+
+        errorMessage.style.display = "block";
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      const errorMessage = document.getElementById("error-message");
+      errorMessage.textContent =
+        "An error occurred while processing your request.";
+      errorMessage.style.display = "block";
+    });
+}
